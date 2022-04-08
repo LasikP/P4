d6e728470f4f3b7025478d935459d06e8d16053e
Patryk Lasik <pl055919@student.ath.edu.pl>
2022-04-08 22:21:47 +0200

    Lab3

---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using System.Data.SqlClient;
+
+string connection = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = Northwind; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+
+var con = new SqlConnection(connection);
+
+var result = con.Query<Region>("SELECT * FROM Region");
+
+foreach (var item in result)
+{
+    Console.WriteLine($"{ item.RegionId}:{ item.RegionDescription}");
+}
+var RegionToInsert = new Region()
+{
+    RegionId = 8,
+    RegionDescription = "TestRegion"
+};
+
+//var insertresult = con.Execute("INSERT INTO Region(RegionID, RegionDescription) VALUES (@RegionID, @RegionDescription)",
+//  new
+//{ RegionID=7,
+//    RegionDescription = "TestAnon"
+//});
+
+//var joinresult = con.Query<Produkt, Category, Produkt>("SELECT * FROM  Products p JOIN Categories c on p.CategoryID = c.CategoryID",
+//    (product,category) =>
+//    {
+//        product.Category = category;
+//        return product;
+//    }, splitOn: "CategoryID");
+//foreach (var item in joinresult)
+//{
+//    Console.WriteLine($"{item.ProductName}:{item.Category.CategoryName}");
+//}
+
+var join2 = con.Query<Region, Territories, Region>("SELECT * FROM Region AS r JOIN Territories AS t on r.RegionID = t.RegionID WHERE TerritoryDescription LIKE 'E%'  ",
+    (region, territories) =>
+    {
+       region.Territories = territories;
+        return region;
+    }, splitOn: "RegionID");
+foreach(var item in join2)
+{
+    Console.WriteLine($"{item.RegionDescription}:{item.Territories.TerritoryDescription}");
+}
+
+
+    
+    
