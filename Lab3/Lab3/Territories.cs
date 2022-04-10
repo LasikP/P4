b4316d67428fea2b59e8dc5e23f8845ea3ad2140
Patryk Lasik <pl055919@student.ath.edu.pl>
2022-04-10 18:47:20 +0200

    Lab3

---
+++ Lab3/Lab3/Territories.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    internal class Territories
+{
+    public int TerritoryID { get; set; }
+    public int TerritoryDescription { get; set; }
+    public int RegionID { get; set; }
+    //public Region Region { get; set; }
+}
+
